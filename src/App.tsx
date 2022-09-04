a9dde2f91c19303c9d609be4a3257fcac61f4aa8
huseyin <huseyin@keyko.io>
2022-09-04 15:46:12 +0300

    showing loginc search and category

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
+import { useLocation } from "react-router-dom";
 import Category from "./components/Category";
 import Header from "./components/Header";
 import Search from "./components/Search";
 import Pages from "./pages/Pages";
 
 const App = () => {
+
+  let location = useLocation();
+
+  const pathRecipe = location.pathname.includes("recipe")
+
   return (
     <div className="App">
       <Header />
-      <Search />
-      <Category />
+      {!pathRecipe && <Search />}
+      {!pathRecipe && <Category />}
       <Pages />
     </div>
   );
